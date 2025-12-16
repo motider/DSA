33b91e3c8eda34d7b5d48ceda616f2cfbb549a78
motider <motider06@gmail.com>
2025-12-16 21:26:34 +0300

    Time: 0 ms (100.00%) | Memory: 13 MB (22.49%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            if nums[i] >= target:
+                return i
+                break
+        else:
+            return n
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
