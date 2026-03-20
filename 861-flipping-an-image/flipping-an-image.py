ca98ca7ced5440fde591879f1dd90656de5714e4
motider <motider06@gmail.com>
2026-03-20 11:36:19 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (19.75%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -6,7 +6,6 @@ class Solution(object):
             left, right = 0, n - 1
             
             while left <= right:
-                # Flip + invert in one step
                 row[left], row[right] = 1 - row[right], 1 - row[left]
                 left += 1
                 right -= 1
