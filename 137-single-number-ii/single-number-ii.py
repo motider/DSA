009da3d04a71a6b471a4255786d91cd2addc1f2d
motider <motider06@gmail.com>
2026-04-04 03:15:45 +0300

    Time: 795 ms (13.19%) | Memory: 13.3 MB (92.93%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        for i in nums:
+            if nums.count(i) == 1:
+                return i
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
