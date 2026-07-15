e6f01bdadd18216bd8364bec3dc597b700283fbe
motider <motider06@gmail.com>
2026-07-15 23:10:00 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (28.00%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        x = n * n
+        y = n * (n + 1)
+
+        return n
+        """
+        :type n: int
+        :rtype: int
+        """
+        
