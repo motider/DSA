1e2fb14a0328f4a7d101f44defa5f9a466ef4499
motider <motider06@gmail.com>
2026-07-20 17:53:38 +0300

    Time: 4 ms (51.23%) | Memory: 12.4 MB (51.28%) - LeetSync

---
+++ 415-add-strings/add-strings.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        x = int(num1) + int(num2)
+        return str(x)
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        
