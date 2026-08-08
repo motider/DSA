02e346345077ebdf6d1d074f0ee4d2be354ab4ac
motider <motider06@gmail.com>
2026-08-08 20:10:57 +0300

    Time: 25 ms (31.98%) | Memory: 16.4 MB (33.25%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,22 +1,17 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        x= []
+        freq = defaultdict(list)
+
         for i in range(len(strs)):
-            y = sorted(strs[i])
-            result = "".join(y)
-            x.append(result)
-        paired = [list(pair) for pair in zip(strs, x)]
-        
-        groups = {} 
-        for i in paired:
-            word = i[0]       
-            sorted_key = i[1] 
-            if sorted_key not in groups:
-                groups[sorted_key] = []
-            
-            groups[sorted_key].append(word)
-        final_result = list(groups.values())
-        return final_result
+            y = "".join(sorted(strs[i]))
+            freq[y].append(strs[i])
+
+        ans = []
+        for i in freq:
+            ans.append(freq[i])
+
+        return ans
+
         """
         :type strs: List[str]
         :rtype: List[List[str]]
