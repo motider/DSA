45d86a5178caa4ca491c9727d6ddc5702b3ee73d
motider <motider06@gmail.com>
2026-08-10 23:10:38 +0300

    Time: 11 ms (41.73%) | Memory: 12.8 MB (10.24%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,15 +1,19 @@
-class Solution:
-    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        y = []
-        for i in zip(heights, names):
-            y.append(i)    
-        print(y)
-        sorted_arr = sorted(y, reverse=True)
-        final = []
-        for h, name in sorted_arr:  
-            final.append(name)
-        return final
-
-
-
+class Solution(object):
+    def sortPeople(self, names, heights):
+        m = {}
+        for i in range(len(heights)):
+            if heights[i] in m:
+                m[heights[i]].append(names[i])
+            else:
+                m[heights[i]] = [names[i]]
+        ans = []
+        for i in sorted(list(m.keys()),reverse = True):
+            m[i].sort()
+            ans.extend(m[i])
+        return ans
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
         
